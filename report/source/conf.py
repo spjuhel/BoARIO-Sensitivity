3754dfc5d42f9424c6b72351d6d790b8f6971c7b
Samuel Juhel <samuel.juhel@zaclys.net>
2023-04-03 19:04:57 +0200

    rst files

---
+++ report/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BoARIO Sensivity Report'
+copyright = '2023, S. Juhel, V. Viguie'
+author = 'S. Juhel, V. Viguie'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_material'
+#html_theme_path = [sphinx_redactor_theme.get_html_theme_path()]
+html_static_path = ['_static']
